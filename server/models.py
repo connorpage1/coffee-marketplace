7f6b12d40acc55291ee300c68e0154f7e10ba4c0
Mason Barnes <barnesmason980@gmail.com>
2024-08-15 11:12:53 -0700

    create order and orderitem tables

--- server/models.py
+++ server/models.py
@@ -4,3 +4,27 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Order(db.model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.Date)
+    total = db.Column(db.Float, nullable=False)
+
+
+    order_items = db.relationship('OrderItem', back_populates='order')
+
+
+class OrderItem(db.model):
+    __tablename__ = 'order_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price_at_order = db.Column(db.Float, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+
+    order = db.relationship('Order', back_populates='order_items')
+    product = db.relationship('Product', back_populates='order_items')
+
+
