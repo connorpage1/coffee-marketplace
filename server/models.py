fc4dad80be975e170a27e98b6c7702cd0e1cbbd5
Mason Barnes <barnesmason980@gmail.com>
2024-08-15 11:55:26 -0700

    fix import issue

--- server/models.py
+++ server/models.py
@@ -3,28 +3,29 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
-class Order(db.model):
+class Order(db.Model):
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
     order_date = db.Column(db.Date)
     total = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String)
+    discount = db.Column(db.Float)
 
 
     order_items = db.relationship('OrderItem', back_populates='order')
 
 
-class OrderItem(db.model):
+class OrderItem(db.Model):
     __tablename__ = 'order_items'
 
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
     price_at_order = db.Column(db.Float, nullable=False)
     order_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
-    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    # product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
 
     order = db.relationship('Order', back_populates='order_items')
-    product = db.relationship('Product', back_populates='order_items')
+    # product = db.relationship('Product', back_populates='order_items')
 
 
