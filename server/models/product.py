ee55379d4fc20fcabd813467a15e3734a56160a0
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-08-15 22:20:21 -0600

    finish validations for product.py

--- server/models/product.py
+++ server/models/product.py
@@ -6,7 +6,7 @@ class Product(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     stock = db.Column(db.String)
-    type = db.Column(db.String, unique=True)
+    type = db.Column(db.String)
     sku = db.Column(db.String)
     image_url = db.Column(db.String)
     description = db.Column(db.String)
@@ -33,64 +33,55 @@ class Product(db.Model, SerializerMixin):
         
     @validates("name")
     def validate_name(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
         elif len(value) < 1:
             raise ValueError(f"{key} must be at least 1 characters long")
-        else:
-            return value
+        return value
         
     @validates("stock")
     def validate_stock(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif not "in stock" and "out of stock":
-            raise ValueError(f"{key} must be either in/out of stock")
-        else:
-            return value
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
+        elif value not in ["in stock", "out of stock"]:
+            raise ValueError(f"{key} must be either 'in stock' of 'out of stock'")
+        return value
         
     @validates("type")
     def validate_type(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif not "coffee" and "tea":
-            raise ValueError(f"{key} must be either type coffee or tea")
-        else:
-            return value
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
+        elif value not in ["coffee", "tea"]:
+            raise ValueError(f"{key} must be either type 'coffee' or 'tea'")
+        return value
         
     @validates("sku")
     def validate_sku(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif not re.match(r'^[A-Z0-9]{8,12}$'):
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
+        elif not re.match(r'^[A-Z0-9]{8,12}$', value):
             raise ValueError(f"{key} must be 8-12 alphanumeric characters long")
-        else:
-            return value
+        return value
         
     @validates("image_url")
     def validate_image_url(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif not re.match(r'^https?:\/\/.*\.(jpg|jpeg|png|gif|bmp|webp|svg)$'):
-            raise ValueError(f"{key} must be a valid url")
-        else:
-            return value
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
+        elif not re.match(r'^https?:\/\/.*\.(jpg|jpeg|png|gif|bmp|webp|svg)$', value):
+            raise ValueError(f"{key} must be a valid image url")
+        return value
         
     @validates("description")
     def validate_description(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif len(value) < 1:
-            raise ValueError(f"{key} must be at least 1 characters long")
-        else:
-            return value
+        if not isinstance(value, str) or not value.strip():
+            raise TypeError(f"{key} must be a non-empty string")
+        elif not (50 <= len(value) <= 1000):
+            raise ValueError("Description must be between 50 and 1000 characters long")
+        return value
         
     @validates("user_id")
     def validate_user_id(self, key, value):
-        if not isinstance(value, str):
-            raise TypeError(f"{key} must be of type str")
-        elif len(value) < 1:
-            raise ValueError(f"{key} must be at least 1 characters long")
-        else:
-            return value
+        if not isinstance(value, int):
+            raise TypeError(f"{key} must be of type int")
+        return value
         
