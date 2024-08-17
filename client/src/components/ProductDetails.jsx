69b8d5ee8070f3faf8731252e731b8b829af7312
Mason Barnes <barnesmason980@gmail.com>
2024-08-17 13:42:58 -0700

    add vendor state variable

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductDetails() {
-    const [product, setProduct] = useState()
-    const { productId}  = useParams();
+    const [product, setProduct] = useState();
+    const [vendor, setVendor] = useState();
+    const { productId }  = useParams();
+    
+    let vendorName
     
     console.log(productId)
     useEffect(() => {
@@ -17,15 +20,23 @@ function ProductDetails() {
         })
         .then((data) => {
           setProduct(data);
+          setVendor(data.user);
         })
         .catch((error) => {
           console.log(error);
         });
     }, [productId]);
 
+    if (vendor) {
+      vendorName = `${vendor.first_name} ${vendor.last_name}`;
+    } else {
+      vendorName = 'No vendor found';
+    }
+
+
 return <div>
     <h1>{product ? product.id : 'No product found'}</h1>
-
+    <h1>{vendorName}</h1>
 </div>;
   }
 
