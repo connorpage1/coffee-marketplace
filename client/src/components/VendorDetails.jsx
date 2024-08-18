4b5eece852be29e5f337efefb8b66e304c3e5633
Mason Barnes <barnesmason980@gmail.com>
2024-08-17 18:39:59 -0700

    add fetch call

--- client/src/components/VendorDetails.jsx
+++ client/src/components/VendorDetails.jsx
@@ -3,6 +3,26 @@ function VendorDetails() {
     const [products, setProducts] = useState();
     const { vendorId } = useParams();
     
+    
+    useEffect(() => {
+        fetch(`/users/${vendorId}`)
+          .then((resp) => {
+            if (resp.ok) {
+              return resp.json();
+            } else {
+              //! Fix Error
+              throw new Error('Error fetching data');
+            }
+          })
+          .then((data) => {
+            setVendor(data);
+            setProducts(data.products);
+          })
+          .catch((error) => {
+          //! Fix Error
+            console.log(error);
+          });
+      }, [vendorId]);
     return <div>
         <h1>7</h1>
     </div>;
