bbeada6c47ff1858a7da3356dfdf2a9726c925a3
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-08-19 14:36:13 -0600

    add handleAddToCart function to ProductPage

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { Container } from 'semantic-ui-react';
+import ProductDetails from "./ProductDetails";
+
+
 function ProductPage() {
+    const [quantity, setQuantity] = useState(1);
+    const [searchQuery, setSearchQuery] = useState("");
+    const { products } = useOutletContext()
+
+    const handleAddToCart = () => {
+        
+        alert(`Added ${quantity} of ${product.name} to cart!`);
+    };
+
+    const finalProducts = productsDisplay
+        .filter((product) => {
+            product.name.toLowerCase().includes(searchQuery.toLocaleLowerCase());
+        })
+        .map((product) => {
+            <ProductDetails 
+                key={product.id}
+                {...product}
+                handleAddToCart={handleAddToCart}
+            />
+        })
     return <div>
         <h1>5</h1>
     </div>;
   }
-
 
   export default ProductPage;
