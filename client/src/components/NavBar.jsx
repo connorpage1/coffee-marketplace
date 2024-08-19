8a659499acc13e2112dd81ce0093bea96c6d4ade
Mason Barnes <barnesmason980@gmail.com>
2024-08-19 13:44:09 -0700

    fix navbar fetch and interpolation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,31 +3,34 @@ import { Link, NavLink } from "react-router-dom"
 
 function NavBar(){
 // if session.get, display logout, else display login
-const [session, setSession] = useState(false)
-// useEffect(() =>{
-//     fetch('/get-session')
-//     .then((res) => {
-//         if (res.ok) {
-//             return res.json();
-//         }
-//         //! Fix Error
-//         else {
-//             console.log('er')
-//         }
-//     })
-//     .then((data) => {
-//         if (data){
-//             setSession('LogOut')
-//         }
-//         else {
-//             setSession('LogIn')
-//         }
-//     })
-//     .catch((error) => {
-//         //! Fix Error
-//         console.log(error);
-//     })
-//     },[]);
+const [endpoint, setEndpoint] = useState(false)
+const [navText, setNavText]  = useState()
+useEffect(() =>{
+    fetch('/get-session')
+    .then((res) => {
+        if (res.status_code == 200 ) {
+            return res.json();
+        }
+        //! Fix Error
+        else {
+            console.log('er')
+        }
+    })
+    .then((data) => {
+        if (data){
+            setEndpoint('logput')
+            setNavText('Log Out')
+        }
+        else {
+            setEndpoint('login')
+            setNavText('Log In')
+        }
+    })
+    .catch((error) => {
+        //! Fix Error
+        console.log(error);
+    })
+    },[]);
 
 return(
 
@@ -37,7 +40,7 @@ return(
 <Link to={'/'}> Home </Link>
 <Link to={'/products'}> Shop </Link>
 <Link to={'/myprofile'}> My Profile</Link>
-<Link to={'/logout'}> Log Out </Link>
+<Link to={`/${endpoint}`}> {navText} </Link>
 <Link to={'/signup'}> Signup </Link>
 </nav>
 </>
