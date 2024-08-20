f9950a0fbfbbbdfe6e1436d9525e6f7eeff470f4
Connor Page <george@connorpage.com>
2024-08-20 12:05:48 -0500

    Est new branch, add useEffect

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 function MyProfile() {
+    useEffect(() => {
+        fetch('/api/v1/profile')
+        .then(res => {
+            if (res.ok) {
+                res.json()
+                .then(profileObj => console.log(profileObj))
+            } else {
+                throw Error("response not ok")
+            }
+        }).catch(err => console.log(`error caught ${err}`))
+    }    
+    , [])
+    
     return (
     <div>
         <h1>3</h1>
