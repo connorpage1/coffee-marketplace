5f8fe7c9d175fb6188e90fbb54e3fe47f895991f
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-08-20 13:39:42 -0600

    create checkout component

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -1,53 +1,44 @@
 import { useEffect, useState } from "react";
-// import { Container } from 'semantic-ui-react';
-import ProductDetails from "./ProductDetails";
-import toast, { Toaster } from "react-hot-toast"
+import { Container } from "semantic-ui-react";
+import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "./SearchBar";
-
+import ProductCard from "./ProductCard";
 
 const ProductPage = () => {
-    // const [quantity, setQuantity] = useState(1);
     const [searchQuery, setSearchQuery] = useState("");
     const [products, setProducts] = useState([]);
-    
+
     useEffect(() => {
-        fetch(`/api/v1/products`)
-        .then(resp => {
-        if (resp.ok) { 
-            resp.json().then(setProducts)
+    fetch(`/products`)
+        .then((resp) => {
+        if (resp.ok) {
+            resp.json().then(setProducts);
         } else {
-            resp.json().then(errorObj => toast.error(errorObj.error))
+            resp.json().then((errorObj) => toast.error(errorObj.error));
         }
-        })
-        .catch(errorObj => toast.error(errorObj.message))
+    })
+        .catch((errorObj) => toast.error(errorObj.message));
     }, []);
 
-    // const handleAddToCart = () => {
-    //     !add functionality here
-    //     alert(`Added ${quantity} of ${product.name} to cart!`);
-    // };
-
     const handleSearch = (e) => {
-        setSearchQuery(e.target.value);
+    setSearchQuery(e.target.value);
     };
-    
-    return (
-        <main>
-            <h1>Coffee and Tea Shop</h1>
-            <SearchBar searchQuery={ searchQuery } handleSearch = { handleSearch} />
-            <div>
-                {products.length > 0 ? (
-                    products.map(product => <ProductDetails key={product.id} product={product} />)
-                ) : (
-                    <p>No products available</p>
-                )}
-            </div>
 
-            {/* <div>
-                {products && products.map(product => <ProductDetails key={product.id} product={product} />)}
-            </div> */}
-        </main>
-    );
+return (
+    <main>
+        <h1>Coffee and Tea Shop</h1>
+        <SearchBar searchQuery={searchQuery} handleSearch={handleSearch} />
+        <div>
+        {products.length > 0 ? (
+            products.map((product) => (
+                <ProductCard key={product.id} {...product} />
+        ))
+        ) : (
+        <p>No products available</p>
+        )}
+        </div>
+    </main>
+);
 };
 
 export default ProductPage;
